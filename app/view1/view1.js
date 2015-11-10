07c9beec135dbf1201306e2e80e99d7642d81c0f
Mahmoud Ali <muddibr@gmail.com>
2015-11-10 00:05:10 -0300

    initial commit

---
+++ app/view1/view1.js
@@ -0,0 +1,38 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
+  }])
+
+  .controller('View1Ctrl', ['$scope', '$http', function ($scope, $http) {
+    $scope.calculos = null;
+    $http.get('view1/calculos.json').success(function (data) {
+      $scope.calculos = data;
+    });
+
+    $scope.parseFloat = function (value) {
+      var temp = parseFloat(value);
+      return isNaN(temp) ? 0 : temp;
+    }
+
+    $scope.toFixed = function (value) {
+      return value.toFixed(0);
+    }
+
+    $scope.total = function (col) {
+      var soma = 0;
+      if ($scope.calculos != null)
+      {
+        for (var index = 0; index < $scope.calculos.length; index++) {
+          soma += parseFloat($scope.calculos[index][col]);
+        }
+      }
+      return soma;
+    }
+
+  }]);
