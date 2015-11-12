c4f9c1c5ae5f4863989b964edfd4fca476698f5b
Mahmoud Ali <muddibr@gmail.com>
2015-11-11 21:07:05 -0300

    Add tsd files

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="angularjs/angular-mocks.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
+/// <reference path="angular-protractor/angular-protractor.d.ts" />
+/// <reference path="selenium-webdriver/selenium-webdriver.d.ts" />
